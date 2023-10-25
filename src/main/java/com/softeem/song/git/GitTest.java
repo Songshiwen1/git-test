e1d73434dfb710df332c7b914cb26f990f07cd07
song <419851415@qq.com>
2023-10-25 17:23:37 +0800

    第二次提交

--- src/main/java/com/softeem/song/git/GitTest.java
+++ src/main/java/com/softeem/song/git/GitTest.java
@@ -2,6 +2,7 @@ package com.softeem.song.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("helloGit");
+        System.out.println("helloGit1");
+        System.out.println("helloGit2");
     }
 }
