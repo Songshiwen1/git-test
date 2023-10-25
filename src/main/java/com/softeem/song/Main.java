b4c47bcce6d85b12c5916c2a4c1df5533cd7c08c
song <419851415@qq.com>
2023-10-25 17:21:48 +0800

    第一次提交

---
+++ src/main/java/com/softeem/song/Main.java
@@ -0,0 +1,7 @@
+package com.softeem.song;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/softeem/song/git/GitTest.java
@@ -0,0 +1,7 @@
+package com.softeem.song.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helloGit");
+    }
+}
